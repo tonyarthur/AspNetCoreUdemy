f165cf97a7b6c1ffb356a5580097dbc2139fdfa5
Tonyarthur <tony_arthur@live.com>
2018-09-01 12:18:01 -0300

    Criando um hosting.

---
+++ Hosting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var  host = new WebHostBuilder()
+               .UseKestrel()
+               .Configure(
+                   app =>{
+                   app.Run(context => context.Response.WriteAsync("Olá mundo"));
+                   }
+               )
+               .Build();
+               host.Run();
+        }
+    }
+}
+
