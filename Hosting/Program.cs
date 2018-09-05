cdd20bbce8141950e3b54b645ce62f9f8dbbfd42
Tonyarthur <tony_arthur@live.com>
2018-09-04 23:06:36 -0300

    Habilitando as configurações de arquivo, do JSON.

--- Hosting/Program.cs
+++ Hosting/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,12 +11,27 @@ namespace Hosting
     {
         static void Main(string[] args)
         {
+          // BuildWebHost(args).Run();
+          
            var  host = new WebHostBuilder()
                .UseKestrel()
                .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                .Build();
+            
                host.Run();
+          
         }
+        //Nova forma de criar um hosting no Dotnet Core 2.0
+        //Repare que agora eu não falo mais o que eu quero em um hosting, eu apenas peço para criar.
+        //Com isso, a nossa classe Startup teve mudanças em seu construtor 
+       /* 
+        private static IWebHost BuildWebHost(string[] args) =>
+        
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
+        */
     }
 }
 
--- Hosting/Startup.cs
+++ Hosting/Startup.cs
@@ -1,17 +1,35 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace Hosting
 {
     public class Startup
     {
+        private IConfigurationRoot _configuration;
+
+        public  Startup(IHostingEnvironment env){
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json");
+
+            builder.AddEnvironmentVariables();
+            _configuration = builder.Build();
+        }
+
+        //public IConfiguration Configuration{get;}
+        
         //Configurando meu Metodo ou função Configure.
         //Passando como parêmto o metodo IApplicationBuilder
         public void Configure(IApplicationBuilder app){
             //Criando uma classe personalizada de middleware. 
             app.UseMiddleware<MyMiddleware>();
-            //Definido um tipo de Middleware
-            app.Run(context => context.Response.WriteAsync("Olá Mundo 2"));
+
+            //Lendo a ApplicationName do arquivo de configuração app.settings.json
+            var applicationName = _configuration.GetValue<string>("ApplicationName");
+            //Definido um tipo de Middlewar
+            app.Run(context => context.Response.WriteAsync($"Olá Mundo 2  {applicationName}"));
         }
     }
 }
